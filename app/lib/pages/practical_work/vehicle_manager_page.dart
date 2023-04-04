ac848512b5fbea648786b10f392ddccb5d9df8ab
Jordan ALCARAZ <jordan.alcaraz.pro@gmail.com>
2023-04-04 18:33:52 +0400

    Q1 : Utiliser un StateProvider pour showAddSection

--- app/lib/pages/practical_work/vehicle_manager_page.dart
+++ app/lib/pages/practical_work/vehicle_manager_page.dart
@@ -1,6 +1,7 @@
 import 'package:app/router/go_router_builder.dart';
 import 'package:app/widgets/app_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uuid/uuid.dart';
 
 class Vehicle {
@@ -23,6 +24,10 @@ bool yearIsValid(String year) {
   return int.tryParse(year) != null;
 }
 
+final showAddSectionProvider = StateProvider<bool>(
+  (ref) => false,
+);
+
 class VehicleManagerPage extends StatefulWidget {
   const VehicleManagerPage({Key? key}) : super(key: key);
 
@@ -32,98 +37,27 @@ class VehicleManagerPage extends StatefulWidget {
 
 class _VehicleManagerPageState extends State<VehicleManagerPage> {
   List<Vehicle> vehicles = [];
-  bool showAddSection = false;
-
-  String name = '';
-  String year = '';
-  String description = '';
 
   @override
   Widget build(BuildContext context) {
-    final canAdd = name.isNotEmpty && yearIsValid(year);
     return Scaffold(
       appBar: AppBar(
         title: const Text('PracticalWork'),
-        actions: [
-          if (showAddSection)
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  showAddSection = false;
-                });
-              },
-              icon: const Icon(Icons.close),
-            )
-          else
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  showAddSection = true;
-                });
-              },
-              icon: const Icon(Icons.add),
-            ),
+        actions: const [
+          _AppBarButton(),
         ],
       ),
       body: Column(
         children: [
-          if (showAddSection)
-            Container(
-              margin: const EdgeInsets.all(16),
-              padding: const EdgeInsets.all(16),
-              color: Colors.tealAccent,
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'Name',
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        name = value;
-                      });
-                    },
-                  ),
-                  TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'Year',
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        year = value;
-                      });
-                    },
-                  ),
-                  TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'Description',
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        description = value;
-                      });
-                    },
-                  ),
-                  AppButton(
-                    text: 'Add',
-                    onPressed: canAdd
-                        ? () {
-                            setState(() {
-                              vehicles.add(
-                                Vehicle(
-                                  id: const Uuid().v1(),
-                                  name: name,
-                                  year: year,
-                                  description: description,
-                                ),
-                              );
-                            });
-                          }
-                        : null,
-                  ),
-                ],
-              ),
-            ),
+          _AddSection(
+            onVehicleAdded: (vehicle) {
+              setState(() {
+                vehicles.add(
+                  vehicle,
+                );
+              });
+            },
+          ),
           const Text('Vehicle list :'),
           Expanded(
             child: ListView.separated(
@@ -150,6 +84,111 @@ class _VehicleManagerPageState extends State<VehicleManagerPage> {
                 );
               },
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends ConsumerWidget {
+  const _AppBarButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final showAddSection = ref.watch(showAddSectionProvider);
+    if (showAddSection) {
+      return IconButton(
+        onPressed: () {
+          ref.read(showAddSectionProvider.notifier).state = false;
+        },
+        icon: const Icon(Icons.close),
+      );
+    }
+
+    return IconButton(
+      onPressed: () {
+        ref.read(showAddSectionProvider.notifier).state = true;
+      },
+      icon: const Icon(Icons.add),
+    );
+  }
+}
+
+class _AddSection extends ConsumerStatefulWidget {
+  const _AddSection({
+    Key? key,
+    required this.onVehicleAdded,
+  }) : super(key: key);
+
+  final void Function(Vehicle vehicle) onVehicleAdded;
+
+  @override
+  ConsumerState<_AddSection> createState() => _AddSectionState();
+}
+
+class _AddSectionState extends ConsumerState<_AddSection> {
+  String name = '';
+  String year = '';
+  String description = '';
+
+  @override
+  Widget build(BuildContext context) {
+    final showAddSection = ref.watch(showAddSectionProvider);
+
+    if (!showAddSection) {
+      return const SizedBox();
+    }
+    final canAdd = name.isNotEmpty && yearIsValid(year);
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
+      color: Colors.tealAccent,
+      child: Column(
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Name',
+            ),
+            onChanged: (value) {
+              setState(() {
+                name = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Year',
+            ),
+            onChanged: (value) {
+              setState(() {
+                year = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Description',
+            ),
+            onChanged: (value) {
+              setState(() {
+                description = value;
+              });
+            },
+          ),
+          AppButton(
+            text: 'Add',
+            onPressed: canAdd
+                ? () {
+                    final vehicle = Vehicle(
+                      id: const Uuid().v1(),
+                      name: name,
+                      year: year,
+                      description: description,
+                    );
+                    widget.onVehicleAdded(vehicle);
+                  }
+                : null,
           ),
         ],
       ),
